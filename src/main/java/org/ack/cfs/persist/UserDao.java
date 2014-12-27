f2bdc0d4bdc1b62566585690ffdb64600002e41c
lzjlvlzj <mxdyxzy@163.com>
2014-12-27 15:29:45 +0800

    Revert "s"
    This reverts commit 89ee16dc8b66fbf1eecb9e4e0d402635c0c44967.

--- src/main/java/org/ack/cfs/persist/UserDao.java
+++
@@ -1,7 +0,0 @@
-package org.ack.cfs.persist;
-
-import org.ack.cfs.entity.User;
-
-public interface UserDao extends BaseDao<User, Integer> {
-
-}
--- src/main/java/org/ack/cfs/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package org.ack.cfs.service;
-
-import org.ack.cfs.entity.User;
-
-public interface UserService extends BaseService<User, Integer> {
-	
-}
